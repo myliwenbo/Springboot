12e3e6b1ceb30c5d41cf5d35956bfbcfea1ed238
myliwenbo <597186288@qq.com>
2019-01-04 10:16:34 +0800

    mybatis+分页插件简单实用

---
+++ datasource/sptingboot-mybatis/src/main/java/vip/xjdai/SptingbootMybatisApplication.java
@@ -0,0 +1,16 @@
+package vip.xjdai;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan ("vip.xjdai.demo")//将项目中对应的mapper类的路径加进来就可以了
+public class SptingbootMybatisApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SptingbootMybatisApplication.class, args);
+    }
+
+}
