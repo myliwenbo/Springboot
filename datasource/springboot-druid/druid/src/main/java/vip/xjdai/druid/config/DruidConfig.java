9f2aa31b6cb574505eda7cfbbd588ebf914c1f89
lwb <myliwenbo@gmail.com>
2019-10-31 17:28:15 +0800

    修改目录结构以及druid和springboo整合

---
+++ datasource/springboot-druid/druid/src/main/java/vip/xjdai/druid/config/DruidConfig.java
@@ -0,0 +1,25 @@
+package vip.xjdai.druid.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * 配置druid需要的配置类，引入application.yml文件中以 <br/>
+ * spring.datasource开头的信息 因此需要在application.yml文件中配置相关信息。
+ *
+ */
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSource druidDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        System.out.println("搭建data");
+        return druidDataSource;
+    }
+}
---
+++ datasource/springboot-druid/druid/src/main/java/vip/xjdai/druid/controller/TestController.java
@@ -0,0 +1,29 @@
+package vip.xjdai.druid.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vip.xjdai.druid.mapper.ScheduleJobMapper;
+import vip.xjdai.druid.model.ScheduleJob;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("test")
+public class TestController
+{
+
+    @Autowired
+    private ScheduleJobMapper scheduleJobMapper;
+
+    @RequestMapping("test")
+    public List< ScheduleJob > test() {
+        ScheduleJob record = new ScheduleJob();
+        record.setJobGroup("more");
+        record.setJobName("1");
+        scheduleJobMapper.insert(record);
+        return scheduleJobMapper.selectByExample(null);
+
+    }
+}
