731cb46a60cc3821ba62c953d23ee8a1e1d30f51
lwb <myliwenbo@gmail.com>
2019-11-13 10:11:00 +0800

    修改目录结构

--- datasource/mybatis/springboot-mybatis-plus-generator/src/main/java/vip/xjdai/generator/CodeGenerator.java
+++ datasource/mybatis/springboot-mybatis-plus-generator/src/main/java/vip/xjdai/generator/CodeGenerator.java
@@ -34,50 +34,6 @@ public class CodeGenerator
         mpg.setTemplate(tc);
         StrategyConfig sc = Config.createStrategyConfig();
         mpg.setStrategy(sc);
-
-
-
-
-//
-        //        // 自定义配置
-        //        InjectionConfig cfg = new InjectionConfig()
-        //        {
-        //            @Override
-        //            public void initMap()
-        //            {
-        //                // to do nothing
-        //            }
-        //        };
-        //
-        //        // 如果模板引擎是 freemarker
-        //        String templatePath = "/templates/mapper.xml.ftl";
-        //        // 如果模板引擎是 velocity
-        //        // String templatePath = "/templates/mapper.xml.vm";
-        //
-        //        // 自定义输出配置
-        //        List< FileOutConfig > focList = new ArrayList<>();
-        //        // 自定义配置会被优先输出
-        //        focList.add(new FileOutConfig(templatePath)
-        //        {
-        //            @Override
-        //            public String outputFile(TableInfo tableInfo)
-        //            {
-        //                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-        //                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName() + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-        //            }
-        //        });
-        //        /*
-        //        cfg.setFileCreate(new IFileCreate() {
-        //            @Override
-        //            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-        //                // 判断自定义文件夹是否需要创建
-        //                checkDir("调用默认方法创建的目录");
-        //                return false;
-        //            }
-        //        });
-        //        */
-        //        cfg.setFileOutConfigList(focList);
-        //        mpg.setCfg(cfg);
         mpg.execute();
     }
 
--- datasource/mybatis/springboot-mybatis-plus-generator/src/main/java/vip/xjdai/generator/Config.java
+++ datasource/mybatis/springboot-mybatis-plus-generator/src/main/java/vip/xjdai/generator/Config.java
@@ -8,11 +8,7 @@ import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.*;
 
 /**
@@ -21,13 +17,24 @@ import java.util.*;
 public class Config
 {
 
-    private static  final String PACKAGE="vip.xjdai";
-
     /**
      * 当前项目路径
      */
-    private static final String path = Thread.currentThread().getContextClassLoader().getResource(
+    private static final String this_path = Thread.currentThread().getContextClassLoader().getResource(
             "").toString().replace("/target/classes/", "").replace("file:/", "");
+
+    /**
+     * 获取parent项目路径
+     */
+    private static final String PATH = System.getProperty("user.dir");
+
+    private static final String PACKAGE      = "vip.xjdai";
+    private static final String MODEL        = PATH + "\\model\\src\\main\\java" + "/com/alijiajituan/model/";
+    private static final String MAPPER       = PATH + "\\dal\\src\\main\\java" + "/com/alijiajituan/dal/mapper/";
+    private static final String WEB          = PATH + "\\web\\src\\main\\java" + "/com/alijiajituan/web/controller/";
+    private static final String XML          = PATH + "\\web\\src\\main\\resources\\mapper\\";
+    private static final String SERVICE      = PATH + "\\service\\src\\main\\java" + "/com/alijiajituan/service/";
+    private static final String SERVICE_IMPL = PATH + "\\service\\src\\main\\java" + "/com/alijiajituan/service/impl/";
 
     /**
      * 包配置
@@ -35,13 +42,13 @@ public class Config
      */
     public static PackageConfig createPackageConfig()
     {
-        PackageConfig pc = new PackageConfig();  // 包配置
-        pc.setParent(PACKAGE);  //父节点
-        pc.setController("controller");//生成的pack名称为 ， vip.xjdai.controller
-        pc.setService("service");//生成的pack名称为 ， vip.xjdai.service
-        pc.setServiceImpl("service.impl");//生成的pack名称为 ， vip.xjdai.service.impl
-        pc.setEntity("domain");//生成的pack名称为 ， vip.xjdai.domain
-        pc.setMapper("mapper");//生成的pack名称为 ， vip.xjdai.mapper
+        PackageConfig pc = new PackageConfig();     //包配置
+        pc.setParent(PACKAGE);                      //父节点
+        pc.setController("web.controller");         //生成的pack名称为
+        pc.setService("service");                   //生成的pack名称为 ，  vip.xjdai.service
+        pc.setServiceImpl("service.impl");          //生成的pack名称为 ，  vip.xjdai.service.impl
+        pc.setEntity("model");                      //生成的pack名称为 ，
+        pc.setMapper("dal.mapper");                 //生成的pack名称为 ，
         return pc;
     }
 
@@ -69,7 +76,7 @@ public class Config
     {
         GlobalConfig gc = new GlobalConfig();
         //        gc.setOutputDir(path+"/src/main/java/"); //设置文件生成的目录
-        gc.setOutputDir(path); //设置文件生成的目录
+        gc.setOutputDir(PATH); //设置文件生成的目录
         gc.setFileOverride(true); //是否覆盖原来的文件
         gc.setAuthor("liwenbo"); //设置作者~
         gc.setOpen(false); //打开输出目录
@@ -102,7 +109,7 @@ public class Config
         strategy.setFieldPrefix("tb_");// 此处可以修改为字段前缀  例如：表 tb_user_name  生成的字段就会 去掉tb_ 最终形成 user_name
         // 公共父类
         //        strategy.setSuperControllerClass("vip.xjdai.common.BaseController");
-        //        strategy.setSuperEntityClass("vip.xjdai.common.BaseEntity"); //自定义继承的Entity类全称
+        //        strategy.setSuperEntityClass("vip.xjdai.common.BaseEntity");        //自定义继承的Entity类全称
         //        strategy.setSuperMapperClass("com.baomidou.ant.common.BaseEntity"); //自定义继承的Mapper类全称，带包名
         strategy.setInclude(scanner("要生成的表")); // 需要生成的表,和排除只能2选一
         //        strategy.setExclude("t_user"); // 需要排除的表，除了排除的表其他的都生成
@@ -155,7 +162,6 @@ public class Config
         }
         throw new MybatisPlusException("请输入正确的" + tip + "！");
     }
-
 
     /**
      * 自定义配置
