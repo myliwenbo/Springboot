f8341aca68a471532d21e27c06391fab7f2f4799
lwb <597186288@qq.com>
2020-12-08 17:23:51 +0800

    添加 swagger

---
+++ springboot/springboot-swagger/swagger2-ui/READMI.md
@@ -0,0 +1,10 @@
+# 非官方UI
+
+
+# 访问
+http://127.0.0.1:8080/doc.html
+
+
+# 参考
+
+> 参考 https://gitee.com/xiaoym/knife4j
---
+++ springboot/springboot-swagger/swagger2-ui/src/test/java/vip/xjdai/swagger2ui/Swagger2UiApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.xjdai.swagger2ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swagger2UiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
